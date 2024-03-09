44b830357e22362161fa602fc32d673d26f83235
Maryan ibrahim <maryanibrahim301@yahoo.com>
2024-03-09 12:56:21 -0600

    create tableOfContents feature

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,13 @@
-import '../sass/main.sass'
+import React from 'react';
+import '../sass/main.sass'; 
+import Nav from '@/components/Nav'; 
+import Layout from '@/components/Layout'; 
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      <Nav />
+    </Layout>
+  );
 }
